8580bf9a3ecfb9376a3acd7c4cd9e3dd7de7063f
Rene Nikodemusz <rene.nikodemusz@porscheinformatik.at>
2019-10-16 14:25:01 +0200

    Some minor changes

--- src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -2,7 +2,6 @@ package guru.springframework.spring5webapp.model;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.math.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
